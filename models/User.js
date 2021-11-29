59d608933512455d66f41e40724d554e2a703b15
wshi27 <wshi27@hotmailcom>
2021-11-29 17:15:09 -0500

    updated

--- models/User.js
+++ models/User.js
@@ -1,15 +1,62 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    name:{
+    firstName:{
         type: String,
         required: true
     },
-    
+    lastName:{
+        type: String,
+        required: true
+    },
+    birthYear:{
+        type: Number,
+        required: true
+    },
+    birthMonth:{
+        type: Number,
+        required: true
+    },
+    birthDay:{
+        type: Number,
+        required: false
+    },
+    streetAddress:{
+        type: String,
+        required: false
+    },
+    cityAddress:{
+        type: String,
+        required: true
+    },
+    stateAddress:{
+        type: String,
+        required: true
+    },
     registeredDate:{
         type: Date,
         required: true,
         default: Date.now
+    },
+    img1:{
+        data: Buffer, 
+        contentType: String 
+    },
+    img2:{
+        data: Buffer,
+        contentType: String
+    },
+    img3:{
+        data: Buffer,
+        contentType: String
+    },
+    img4:{
+        data: Buffer,
+        contentType: String
+    },
+    img5:{
+        data: Buffer,
+        contentType: String
     }
 })
 
